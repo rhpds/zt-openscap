13b7592768c91b01deef714413b860c7ad99a68e
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-07-16 14:26:26 -0500

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-yum remove -y google-rhui-client-rhel8.noarch
 
-yum -y install httpd
+firewall-cmd --zone=public --add-service=https --permanent
+firewall-cmd --reload
 
-firewall-cmd --permanent --zone=public --add-service=http
-systemctl reload firewalld
 
-systemctl enable --now httpd
+#yum remove -y google-rhui-client-rhel8.noarch
+
+#yum -y install httpd
+
+#firewall-cmd --permanent --zone=public --add-service=http
+#systemctl reload firewalld
+
+#systemctl enable --now httpd
+
+#firewall-cmd --zone=public --add-service=https --permanent
+#firewall-cmd --reload
 
 # Create rhel user and set password
 # sudo useradd -m rhel && echo "rhel:redhat" | sudo chpasswd && sudo usermod -aG wheel rhel
