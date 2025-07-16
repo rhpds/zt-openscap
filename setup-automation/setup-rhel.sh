90a4530559a0cab597c4b30210be027ea8920e01
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-07-16 14:11:40 -0500

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -7,3 +7,6 @@ firewall-cmd --permanent --zone=public --add-service=http
 systemctl reload firewalld
 
 systemctl enable --now httpd
+
+# Create rhel user and set password
+sudo useradd -m rhel && echo "rhel:redhat" | sudo chpasswd && sudo usermod -aG wheel rhel
