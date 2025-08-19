047d5f58cd86a9aa04114d38a93b467f1f4a03eb
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-08-18 23:35:14 -0700

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -3,6 +3,10 @@
 firewall-cmd --zone=public --add-service=https --permanent
 firewall-cmd --reload
 
+echo "[WebService]" > /etc/cockpit/cockpit.conf
+echo "Origins = https://cockpit-$(hostname -f|cut -d"-" -f2).apps.$(grep search /etc/resolv.conf| grep -o '[^ ]*$')" >> /etc/cockpit/cockpit.conf
+echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
+systemctl enable --now cockpit.socket
 
 #yum remove -y google-rhui-client-rhel8.noarch
 
